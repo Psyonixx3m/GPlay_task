1b6dfe0fbb8263aa20f3728eca608e9692c5f666
Teo Todorov <theo.tod@hotmail.com>
2021-06-10 22:05:44 +0300

    Add DataBase

--- gplay_crawler/spiders/gplay_crawler.py
+++ gplay_crawler/spiders/gplay_crawler.py
@@ -1,10 +1,15 @@
 import scrapy
 from gplay_crawler.items import GplayCrawlerItem
+import sqlite3
+
+conn = sqlite3.connect('gplay.db')
+c = conn.cursor()
+
+c.execute('''CREATE TABLE gplaydata(gp_cat TEXT, gp_sub_cat TEXT, gp_title TEXT, gp_sub_title TEXT, gp_art_num TEXT, pg_price REAL)''')
 
 
 class GPlayCrawler(scrapy.Spider):
     name = "gp_items"
-    cnt = 0
     start_urls = [
                 "https://gplay.bg/%D0%B3%D0%B5%D0%B9%D0%BC%D0%B8%D0%BD%D0%B3-%D1%85%D0%B0%D1%80%D0%B4%D1%83%D0%B5%D1%80",
                 "https://gplay.bg/%D0%B3%D0%B5%D0%B9%D0%BC%D0%B8%D0%BD%D0%B3-%D0%BF%D0%B5%D1%80%D0%B8%D1%84%D0%B5%D1%80%D0%B8%D1%8F",
@@ -23,9 +28,7 @@ class GPlayCrawler(scrapy.Spider):
         next_page = resp.xpath("//a[@rel='next']")
         if next_page:
             next_page_url = next_page.xpath("./@href").get()
-            if self.cnt < 1:
-                self.cnt += 1
-                yield scrapy.Request(url=next_page_url, callback=self.parse_item)
+            yield scrapy.Request(url=next_page_url, callback=self.parse_item)
 
     def parse_item(self, resp):
         gp_avail = resp.xpath("//span[@title='наличен']").get()
@@ -36,7 +39,7 @@ class GPlayCrawler(scrapy.Spider):
             gp_sub_cat = resp.xpath("//div[@class='path py-3']//a[contains(text(),'')][3]//text()").get()
             gp_title = resp.xpath("//h1[@class='large-title']//text()").get()
             gp_sub_title = resp.xpath("//h2[@class='product-subtitle ']//text()").get()
-            gp_art_num = resp.xpath("//div[contains(@class,'product-ref-number')]//text()").get()
+            gp_art_num = resp.xpath("//div[@class='col-md-6 product-ref-number']//strong//text()").get()
             pg_price = float(resp.xpath("//div[contains(@class, 'product-price-controls')]//price/@*").get())
             if pg_price < 200:
                 item = GplayCrawlerItem()
@@ -46,4 +49,7 @@ class GPlayCrawler(scrapy.Spider):
                 item["gp_sub_title"] = gp_sub_title
                 item["gp_art_num"] = gp_art_num
                 item["pg_price"] = pg_price
+                c.execute(''' INSERT INTO gplaydata(gp_cat, gp_sub_cat, gp_title, gp_sub_title, gp_art_num, pg_price) VALUES(?,?,?,?,?,?)''',(gp_cat, gp_sub_cat, gp_title, gp_sub_title, gp_art_num, pg_price))
+                conn.commit()
                 yield item
+
